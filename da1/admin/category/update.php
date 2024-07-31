a8e4eb8e3a3bc0406618c3414cb37d3e98878144
buitruong1234 <truongbqph26905@fpt.edu.vn>
2024-07-31 22:26:37 +0700

    c

--- da1/admin/category/update.php
+++ da1/admin/category/update.php
@@ -6,7 +6,7 @@
 
 <form action="index.php?act=updatedm" method="post" style="margin-left:260px;">
     <div class="card-body">
-        <h1>Cập nhật danh mụcccccccc</h1>
+        <h1>Cập nhật danh mục</h1>
         <input type="text" name="tenloai" class="form-control" placeholder="TÊN DANH MỤC" value="<?php if(isset($name) && ($name!="")) echo $name ?>">
     </div>
     <div class="card-body">
